9dc2b6763da82d2da2fe3fc0f8fa229b310dd4ed
ali aharian <aliaharian@alis-MacBook-Pro.local>
2024-01-21 11:41:35 +0330

    remove google analytics

--- resources/views/includes/headLinks.blade.php
+++ resources/views/includes/headLinks.blade.php
@@ -59,8 +59,8 @@
 <link rel="stylesheet" href="/packages/owl/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="/packages/owl/assets/owl.theme.default.min.css">
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110406743-1"></script>
+{{--<!-- Global site tag (gtag.js) - Google Analytics -->--}}
+{{--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110406743-1"></script>--}}
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
