b445047e2831c6a0697339ce9a0e3c81eba68235
ali aharian <aliaharian@alis-MacBook-Pro.local>
2024-01-21 11:22:40 +0330

    fixing all ip getches2

--- app/Http/Middleware/orders.php
+++ app/Http/Middleware/orders.php
@@ -9,8 +9,8 @@ class orders
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -22,20 +22,15 @@ class orders
                 $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
                 $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
             }
-            $client  = @$_SERVER['HTTP_CLIENT_IP'];
+            $client = @$_SERVER['HTTP_CLIENT_IP'];
             $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-            $remote  = $_SERVER['REMOTE_ADDR'];
+            $remote = $_SERVER['REMOTE_ADDR'];
 
-            if(filter_var($client, FILTER_VALIDATE_IP))
-            {
+            if (filter_var($client, FILTER_VALIDATE_IP)) {
                 $ip = $client;
-            }
-            elseif(filter_var($forward, FILTER_VALIDATE_IP))
-            {
+            } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
                 $ip = $forward;
-            }
-            else
-            {
+            } else {
                 $ip = $remote;
             }
 
@@ -46,10 +41,10 @@ class orders
         $agent = new \Jenssegers\Agent\Agent();
         $user_platform = $agent->platform();
         $user_browser = $agent->browser();
-        $pre_order=\App\Pre_order::where('user_ip',$user_ip)->where('user_platform',$user_platform)->where('user_browser',$user_browser)->get();
+        $pre_order = \App\Pre_order::where('user_ip', $user_ip)->where('user_platform', $user_platform)->where('user_browser', $user_browser)->get();
 
-        if ($pre_order->count()==0){
-            return redirect('/product');
+        if ($pre_order->count() == 0) {
+            return redirect('/product?ip=' . $user_ip . '&platform=' . $user_platform . '&browser=' . $user_browser);
         }
 
         return $next($request);
