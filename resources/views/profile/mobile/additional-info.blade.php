bf9d1891ffa1d25492b016d6f63bc18360dafc67
ali aharian <aliaharian@alis-MacBook-Pro.local>
2024-01-21 11:46:47 +0330

    config inputmask

--- resources/views/profile/mobile/additional-info.blade.php
+++ resources/views/profile/mobile/additional-info.blade.php
@@ -109,7 +109,7 @@
 <!-- Footer -->
 @include('includes.footer')
 @include('includes.footerLinks')
-<script type='text/javascript' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/jquery.inputmask.bundle.js"></script>
+<script type='text/javascript' src="https://statics.arastowel.com/js/jquery.inputmask.bundle.js"></script>
 
 <script>
 
