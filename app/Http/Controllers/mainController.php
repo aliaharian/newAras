eaf334268919156f93a3614d3bc1e1274553d285
ali aharian <aliaharian@alis-MacBook-Pro.local>
2024-02-20 15:57:17 +0330

    paymeny

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -889,7 +889,54 @@ class mainController extends Controller
                 $TokenArray = $this->makeHttpChargeRequest('POST', $dataQuery, $AddressServiceToken);
                 $decode_TokenArray = json_decode($TokenArray);
 
-                dd($decode_TokenArray);
+//                dd($decode_TokenArray);
+                $Status = $decode_TokenArray->Status;
+                $AccessToken = $decode_TokenArray->Accesstoken;
+
+
+                if (!empty($AccessToken) && $Status == 0) {
+                    $AddressIpgPay = "https://sepehr.shaparak.ir:8080/pay";
+
+                    ?>
+
+
+                    <?php
+                    sleep(10);
+                    ?>
+
+                    <form action="<?php echo $AddressIpgPay; ?>" method="POST" id="ipg">
+                        <input name="TerminalID" type="hidden" required="required"
+                               value="98610186">
+
+                        <input name="token" type="hidden" required="required" value="<?php echo $AccessToken; ?>">
+
+                        //select between GET = 1 or POST = 0
+                        <input name="getMethod" type="hidden" required="required" value="1">
+
+                        <!-- <input type="submit"/> -->
+
+                    </form>
+
+                    <script type="text/javascript">
+                        document.getElementById('ipg').submit(); // SUBMIT FORM
+                    </script>
+                    <?php
+                }else {
+
+                    $error_message = "  دریافت توکن با خطا مواجه شد !!! "
+                    ?>
+
+                    <form action="<?php echo route('pay-from-zarrin', [$invoice_number]); ?>" method="GET" id="error_token">
+                        <input name="error_message" type="hidden" required="required" value="<?php echo $error_message; ?>">
+                    </form>
+
+                    <script type="text/javascript">
+                        document.getElementById('error_token').submit(); // SUBMIT FORM
+                    </script>
+                    <?php
+
+                }
+
 
 //                $results = Zarinpal::request(
 //                    route('pay-from-zarrin', [$invoice_number]),
@@ -1016,7 +1063,8 @@ class mainController extends Controller
 
     }
 
-    public function makeHttpChargeRequest($_Method, $_Data, $_Address){
+    public function makeHttpChargeRequest($_Method, $_Data, $_Address)
+    {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $_Address);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $_Method);
